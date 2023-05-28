a305060b6fe5adbf427699e5bf9207c892c23af2
magnetenstad <tenstad.magne@gmail.com>
2023-05-28 15:16:26 +0200

    Move files to src

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,12 @@ import { oakCors } from 'https://deno.land/x/cors@v1.2.2/mod.ts';
 import { config } from 'https://deno.land/x/dotenv@v3.2.2/mod.ts';
 
 export const env = config({ export: true });
-
 const app = new Application();
 const PORT = 8000;
+
 app.use(oakCors()); // TODO: Dangerous?
 app.use(router.routes());
 app.use(router.allowedMethods());
+
 console.log(`Listening on port ${PORT}`);
 await app.listen({ port: PORT });
